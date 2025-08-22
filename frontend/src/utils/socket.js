c00bfd101c30c86b45d5ce781f57836c542cf101
Sarthak <s.sarthak1357@gmail.com>
2025-08-22 14:26:19 +0530

    updating the backend url

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const token = localStorage.getItem("whiteboard_user_token");
 
-const socket = io("https://api-whiteboard-az.onrender.com", {
+const socket = io("https://whiteboard-seven-bay.vercel.app", {
   extraHeaders: token ? { Authorization: `Bearer ${token}` } : {}, // Only send if token exists
 });
 
